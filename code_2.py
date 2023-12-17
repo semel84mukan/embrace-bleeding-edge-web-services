ca7a0c5bfb07928fe10db9764606af61145bc0de
semel84mukan <152295085+semel84mukan@users.noreply.github.com>
2023-12-17 03:14:24 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,51 @@
+    return data
+def generate_random_data():
+    for item in data:
+import random
+
+def main():
+    data = generate_random_data()
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    main()
+        int[] data = new int[10];
+    }
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+}
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+        for (int item : data) {    }
+    public static void main(String[] args) {
+        }
+        int[] data = new int[10];
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+
+            System.out.println("Random Number: " + item);
+        }
+        for (int item : data) {    for item in data:
+import random
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+
+
+if __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")
+
+    return data
